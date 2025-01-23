e92bbebef3c431bc1cda6355d26cf06e1858c169
suhaim km <suhaimkm441@gmail.com>
2025-01-23 10:55:01 +0530

    updated

--- array.ts
+++ array.ts
@@ -5,6 +5,8 @@ console.log(`sum =${sum}`);
 let product:number = numbers.reduce((product:number, n:number) => product * n);
 console.log(`product =${product}`);
 
+let stringnumbers :string[] = numbers.map(n=> n.toString());
 
-
-
+let str:string='';
+let concat:string = stringnumbers.reduce((str, n:string) => str + '-' + n);
+console.log(concat);
